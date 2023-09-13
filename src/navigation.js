bc4a1fd237649345312c5d0a9633aa25b93dc7a1
HuntHub <hunter.hartnett.moseley@gmail.com>
2023-09-13 18:24:13 -0400

    Small code error change

--- src/navigation.js
+++ src/navigation.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Navbar, Nav, Button } from 'react-bootstrap';
+import { Navbar, Button } from 'react-bootstrap';
 import { Auth } from 'aws-amplify';
 
 const Navigation = () => {
